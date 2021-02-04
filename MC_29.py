612591364f5b1bad4fd1ad91f1924a3c8ec2dab9
Rich Colburn <the3dadvantage@gmail.com>
2021-02-04 10:54:03 -0800

    Update MC_29.py

--- MC_29.py
+++ MC_29.py
@@ -2843,6 +2843,10 @@ def surface_follow(cloth, avatar, value):
     avk = avatar.data.shape_keys.key_blocks['Armature']
     avk.value = value
 
+    if 'Armature.001' in avatar.data.shape_keys.key_blocks:
+        avk2 = avatar.data.shape_keys.key_blocks['Armature.001']
+        avk2.value = 1 - value
+    
     bpy.context.scene.MC_props.interference = True
     
 
@@ -2901,7 +2905,7 @@ def spring_basic_no_sw(cloth):
             surface_follow(cloth, colliders[0], 0.6)
             cloth.velocity[:] = 0.0
             
-            cloth.ob.MC_props.bend_iters = 1
+            cloth.ob.MC_props.bend_iters = 4
             cloth.ob.MC_props.gravity = -1.0
             cloth.ob.MC_props.sew_force = .5
             cloth.iterator += 1
@@ -2920,7 +2924,7 @@ def spring_basic_no_sw(cloth):
     
         if cloth.iterator == 190:
             surface_follow(cloth, colliders[0], 0.0)
-            cloth.ob.MC_props.velocity = 0.99
+            cloth.ob.MC_props.velocity = 0.98
             cloth.iterator += 1
             return                        
         
@@ -2994,7 +2998,7 @@ def spring_basic_no_sw(cloth):
     #v_move = cloth.co - cloth.vel_zero
     rt_(num='stretch time')
         
-    T = time.time()
+
     if cloth.ob.MC_props.detect_collisions:
         colliders = [o for o in bpy.data.objects if (o.MC_props.collider) & (cloth.ob != o)]
         if len(colliders) > 0:
@@ -3093,7 +3097,6 @@ def spring_basic_no_sw(cloth):
             # --------------------------------
             rt_(num='object collisions sw')        
     # self collistion ---------------------------
-    print(time.time() - T, "sc")
     rt_(None, False)
     if cloth.ob.MC_props.self_collide:
         #if cloth.ob.data.is_editmode:
@@ -3468,29 +3471,12 @@ def cloth_physics(ob, cloth):#, colliders):
             else:
                 cache(cloth)
 
-        #update_shading = True
-        #update_shading = False
-        if bpy.context.scene.MC_props.update_shading: # for live shading update        
-            obm = cloth.obm
-            obm.faces.ensure_lookup_table()
-            obm.edges.ensure_lookup_table()
-            vsel = obm.verts[0].select
-            fsel = obm.faces[0].select
-            esel = obm.edges[0].select
-            obm.verts[0].select = True
-            bpy.ops.mesh.hide()
-            bpy.ops.mesh.reveal()
-            obm.verts[0].select = vsel
-            obm.faces[0].select = fsel
-            obm.edges[0].select = esel
-
         return
 
     # switched out of edit mode
     if cloth.mode is None:
         cloth.mode = 1
-
-        #print("running here")
+        
         #refresh(cloth, skip=True)
         #index = ob.data.shape_keys.key_blocks.find('MC_current')
         #ob.active_shape_key_index = index
@@ -4752,10 +4738,6 @@ class McPropsScene(bpy.types.PropertyGroup):
     bpy.props.BoolProperty(name="View Virtual Springs", description="create a mesh to show virtual springs", default=False)
     # make this one a child object that is not selectable.
 
-    update_shading:\
-    bpy.props.BoolProperty(name="Update Shading in Edit Mode", description="Bad for performance but keeps eevee shading updated", default=False)
-    # make this one a child object that is not selectable.
-
 
 # ^                                                          ^ #
 # ^                     END properties                       ^ #
@@ -4907,11 +4889,6 @@ def update_v_norms(cloth):
     # now get vertex normals with add.at
     cloth.v_norms[:] = 0.0
     np.add.at(cloth.v_norms, cloth.v_norm_indexer, normals[cloth.v_norm_indexer1])
-    dots = np.sqrt(np.einsum('ij,ij->i', cloth.v_norms, cloth.v_norms))[:, None]
-    cloth.v_norms /= dots
-    #cloth.v_norms *= 3
-    #print(cloth.v_norms[1] @ cloth.v_norms[0])
-    #print(np.unique(cloth.v_norm_indexer))
 
 
 def refresh(cloth, skip=False):
@@ -5024,7 +5001,8 @@ def refresh(cloth, skip=False):
     cloth.sc_co = np.empty((cloth.co.shape[0] * 2, 3), dtype=np.float32)
 
     for i, j in enumerate(cloth.obm.verts):
-        if j.is_boundary:
+        if j.is_boundary:    
+            print('boundary')
             cloth.group_surface_offset[i] = -0.1
 
     Collider(cloth)
@@ -5726,7 +5704,6 @@ class PANEL_PT_modelingClothPreferences(bpy.types.Panel):
         col.scale_y = 1
         col.label(text='Preferences')
         col.prop(sc.MC_props, "run_editmode", text="Editmode Run")
-        col.prop(sc.MC_props, "update_shading", text="Update Shading")
         col.prop(sc.MC_props, "pause_selected", text="Pause Selected")
         col.prop(sc.MC_props, "view_virtual", text="View Virtual Springs")
 
